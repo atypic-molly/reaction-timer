dc39d878822e80e33e1500158394bc1b40a97834
Molly Brown <mollybrown@Mollys-MacBook-Pro.local>
2024-05-22 14:53:53 -0400

    correction to play button

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,14 @@
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">play</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame" />
-  <p>Reaction time: {{ score }} ms</p>
+  <p v-if="showResults">Reaction time: {{ score }} ms</p>
 </template>
 <!--why is :disabled="is Playing" not ===true??-->
 
 <!--data is an object that returns functions-->
 <script>
 import Block from './components/Block.vue'
+
 export default {
   name: 'App',
   components: { Block },
@@ -16,17 +17,21 @@ export default {
     return {
       isPlaying: false,
       delay: null,
-      score: null
+      score: null,
+      showResults: false
     }
   },
   methods: {
     start() {
+      //set time amount (ms)
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true
+      this.showResults = false
     },
     endGame (reactionTime) {
       this.score = reactionTime
-      this.isPlaying = true
+      this.isPlaying = false
+      this.showResults = true
     }
   }
 }
