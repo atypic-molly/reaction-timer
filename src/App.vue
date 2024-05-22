4ccf3abbcae6c797a7c452f96e5fa7a18fdccb84
Molly Brown <mollybrown@Mollys-MacBook-Pro.local>
2024-05-22 12:09:20 -0400

    delay setting

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
+  <button @click="start">play</button>
 </template>
 
+<!--data is an object that returns functions-->
 <script>
 export default {
   name: 'App',
-  components: {}
+  components: { },
+  
+  data() {
+    return {
+      isPLaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPLaying = true
+      console.log(this.delay)
+    }
+  }
 }
 </script>
 
